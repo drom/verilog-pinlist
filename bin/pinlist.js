759d2993dc9767619946e2bc4b6cef200d10558d
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2018-12-10 18:33:10 -0800

    initial code

---
+++ bin/pinlist.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+'use strict';
+
+const yargs = require('yargs');
+const fs = require('fs-extra');
+const path = require('path');
+const util = require('util');
+
+const lib = require('../lib/index.js');
+
+const readFile = util.promisify(fs.readFile);
+
+const pinlister = lib();
+
+const argv = yargs
+    .option('verbose', {
+        alias: 'v',
+        default: false
+    })
+    .help()
+    .argv;
+
+(async (files) => {
+
+  files.map(async file => {
+    const pat = path.resolve(process.cwd(), file);
+    const source = await readFile(pat, 'utf-8');
+    console.log(source);
+    console.log(pinlister(source));
+  })
+
+})(argv._);
+// console.log(pinlister(source));
---
+++ test/ansi.v
@@ -0,0 +1,7 @@
+module ansi (
+    input [7:0] a, d,
+    output logic [7:0] q,
+    input clk, reset_n
+);
+
+endmodule
