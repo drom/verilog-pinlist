0b82c368d03255c0fc1b243e481cb51a771f67c7
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-11-20 23:51:08 -0800

    online test fix

--- test/online.js
+++ test/online.js
@@ -14,7 +14,7 @@ const links = {
   'https://raw.githubusercontent.com/parallella/oh/master/src/aes/hdl/aes_128.v': {aes_128: 4, expand_key_128: 5},
   'https://raw.githubusercontent.com/parallella/oh/master/src/accelerator/hdl/axi_accelerator.v': {axi_accelerator: 81},
   'https://raw.githubusercontent.com/parallella/oh/master/src/spi/hdl/axi_spi.v': {axi_spi: 50},
-  'https://raw.githubusercontent.com/analogdevicesinc/hdl/master/library/axi_ad9963/axi_ad9963.v': {axi_ad9963: 48},
+  'https://raw.githubusercontent.com/analogdevicesinc/hdl/master/library/axi_ad9963/axi_ad9963.v': {axi_ad9963: 51},
   'https://raw.githubusercontent.com/analogdevicesinc/hdl/master/library/axi_ad9963/axi_ad9963_rx.v': {axi_ad9963_rx: 29},
 };
 
