244708981357cdd0c82287bbf3e814f2705fb59f
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-09-13 13:36:31 -0700

    :arrow_up: dependencies

--- test/online.js
+++ test/online.js
@@ -10,10 +10,9 @@ const expect = chai.expect;
 const pl = lib();
 
 const links = {
-  'https://raw.githubusercontent.com/parallella/oh/master/src/axi/hdl/emaxi.v': {emaxi: 49},
-  'https://raw.githubusercontent.com/parallella/oh/master/src/aes/hdl/aes_128.v': {aes_128: 4, expand_key_128: 5},
-  'https://raw.githubusercontent.com/parallella/oh/master/src/accelerator/hdl/axi_accelerator.v': {axi_accelerator: 81},
-  'https://raw.githubusercontent.com/parallella/oh/master/src/spi/hdl/axi_spi.v': {axi_spi: 50},
+  'https://raw.githubusercontent.com/aolofsson/oh/master/axi/hdl/emaxi.v': {emaxi: 49},
+  'https://raw.githubusercontent.com/aolofsson/oh/master/edma/hdl/edma.v': {edma: 17},
+  'https://raw.githubusercontent.com/aolofsson/oh/master/spi/hdl/axi_spi.v': {axi_spi: 50},
   'https://raw.githubusercontent.com/analogdevicesinc/hdl/master/library/axi_ad9963/axi_ad9963.v': {axi_ad9963: 51},
   'https://raw.githubusercontent.com/analogdevicesinc/hdl/master/library/axi_ad9963/axi_ad9963_rx.v': {axi_ad9963_rx: 29},
 };
